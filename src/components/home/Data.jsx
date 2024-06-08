776c80a7936f286b35deacbae959968c28540948
Ishan0704 <ishan2003dixit@gmail.com>
2024-06-08 14:28:48 +0530

    Responsive home

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Data = () => {
   return (
     <div className="home__data">
-        <h1 className="home__title">Priya Rakibe
+        <h1 className="home__title">
+        Priya Rakibe
         <svg
                   width="36"
                   height="36"
